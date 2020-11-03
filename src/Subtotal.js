07a2a5a97e41b7a11c396a4e6a4819c1430a5006
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2020-11-03 13:38:13 -0800

    fixed some bootstrap classes causing conflicts, checkout setup, working on stripe payment method

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -3,10 +3,11 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 import { useStateValue } from "./StateProvider";
 import { getBasketTotal } from "./reducer";
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
+  const history = useHistory();
   const [{ basket }] = useStateValue();
-  console.log(basket);
 
   return (
     <div className="subtotal">
@@ -28,7 +29,9 @@ function Subtotal() {
         thousandSeperator={true}
         prefix={"$"}
       />
-      <button>Proceed to Checkout</button>
+      <button onClick={(e) => history.push("/payment")}>
+        Proceed to Checkout
+      </button>
     </div>
   );
 }
