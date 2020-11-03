dce80ddc3458bab85667d2dc8c600d7537cb82f4
Phil Laub <PHILLIP.LAUB.EVO@GMAIL.COM>
2020-11-03 11:35:51 -0800

    added Carousel effect to home background image

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./Home.css";
 import Product from "./Product";
+import HomeCarousel from "./HomeCarousel";
 
 function Home() {
   return (
     <div className="home">
       <div className="home__container">
-        <img
+        {/* Carousel start */}
+
+        {/* <img
           className="home__image"
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220.jpg"
           alt=""
-        />
+        /> */}
+        <HomeCarousel className="home__image" />
+        {/* Carousel End */}
 
         <div className="home__row">
           <Product
